969d3da53a020ad8f88146a1bcb3a649da8dea7e
ben <kellman.ben@gmail.com>
2021-08-14 21:34:11 -0700

    Removing glfwWindowHint for GLFW_OPENGL_PROFILE due to incompatibility with lab computers

--- include/window.hpp
+++ include/window.hpp
@@ -32,7 +32,6 @@ public:
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, resizable);
     window_ = glfwCreateWindow(width, height, title.c_str(), nullptr, nullptr);
 
