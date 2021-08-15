16dc6609245c0bf9b7e53517c94acd6d0fd7b842
ben <kellman.ben@gmail.com>
2021-08-14 21:15:11 -0700

    Removing glfw minor version window hint

--- include/window.hpp
+++ include/window.hpp
@@ -30,7 +30,6 @@ public:
         height_(height) {
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, resizable);
